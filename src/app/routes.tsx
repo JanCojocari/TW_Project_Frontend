385679b6b582e41efd2325d5d9e341dc8d643675
Cojocari Jan <jan.cojocari4@gmail.com>
2026-02-09 20:54:23 +0200

    Header is created

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -2,8 +2,8 @@
 import Home from "../pages/Home"
 import Login from "../pages/Login";
 export const publicRoutes = [
-    {path: "/", element: <Login />},
-    // {path: "/home", element: <Home/>},
+    {path: "/*", element: <Login />},
+    {path: "/home", element: <Home/>},
     {path: "/login", element: <Login/>}
 ];
 
